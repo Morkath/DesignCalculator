d6e24b51635a34608805560f5c873a8aa843736a
Morkath <travispijut81@hotmail.com>
2023-01-03 05:49:06 -0600

    Hooking up the UI

---
+++ DesignCalcForm.cs
@@ -0,0 +1,404 @@
+namespace DesignCalculator
+{
+    public partial class DesignCalcForm : Form
+    {
+        public Program myProgram;
+        public bool update = true;
+
+        public int UnitType
+        {
+            get { return combo_unitType.SelectedIndex; }
+            set { combo_unitType.SelectedIndex = value; }
+        }
+        public int Techlevel
+        {
+            get { return combo_techLevel.SelectedIndex; }
+            set { combo_techLevel.SelectedIndex = value; }
+        }
+        public int ProjectType
+        {
+            get { return combo_projectType.SelectedIndex; }
+            set { combo_projectType.SelectedIndex = value; }
+        }
+        public int Plans
+        {
+            get { return combo_plans.SelectedIndex; }
+            set { combo_plans.SelectedIndex = value; }
+        }
+
+        public decimal SimulationCount
+        {
+            get { return num_runSimsCount.Value; }
+            set { num_runSimsCount.Value = value; }
+        }
+        public decimal CompileCount
+        {
+            get { return num_compileDesignCount.Value; }
+            set { num_compileDesignCount.Value = value; }
+        }
+        public decimal ResearchTakeSevenCount
+        {
+            get { return num_researchTakeSevenCount.Value; }
+            set { num_researchTakeSevenCount.Value = value; }
+        }
+        public decimal ResearchCriticalFailCount
+        {
+            get { return num_researchCriticalFailCount.Value; }
+            set { num_researchCriticalFailCount.Value = value; }
+        }
+        public decimal BuildPrototypeCount
+        {
+            get { return num_buildPrototypeCount.Value; }
+            set { num_buildPrototypeCount.Value = value; }
+        }
+        public decimal FieldTestCount
+        {
+            get { return num_fieldTestCount.Value; }
+            set { num_fieldTestCount.Value = value; }
+        }
+        public decimal WorkOutIssuesCount
+        {
+            get { return num_workOutIssuesCount.Value; }
+            set { num_workOutIssuesCount.Value = value; }
+        }
+        public decimal DevelopTakeSevenCount
+        {
+            get { return num_developTakeSevenCount.Value; }
+            set { num_developTakeSevenCount.Value = value; }
+        }
+        public decimal DevelopCriticalFailCount
+        {
+            get { return num_developCriticalFailCount.Value; }
+            set { num_developCriticalFailCount.Value = value; }
+        }
+
+        public string BaseTN
+        {
+            get { return tb_baseTN.Text; }
+            set { tb_baseTN.Text = value; }
+        }
+        public string BaseTP
+        {
+            get { return tb_baseTP.Text; }
+            set { tb_baseTP.Text = value; }
+        }
+        public string SPCost
+        {
+            get { return tb_spCost.Text; }
+            set { tb_spCost.Text = value; }
+        }
+        public string TotalTP
+        {
+            get { return tb_totalTP.Text; }
+            set { tb_totalTP.Text = value; }
+        }
+        public string MonthsTaken
+        {
+            get { return tb_monthsTaken.Text; }
+            set { tb_monthsTaken.Text = value; }
+        }
+        public string SortiesTaken
+        {
+            get { return tb_sorties.Text; }
+            set { tb_sorties.Text = value; }
+        }
+        public string ResearchTN
+        {
+            get { return tb_researchTN.Text; }
+            set { tb_researchTN.Text = value; }
+        }
+        public string ResearchTP
+        {
+            get { return tb_researchExtraTP.Text; }
+            set { tb_researchExtraTP.Text = value; }
+        }
+        public string DevelopTN
+        {
+            get { return tb_developmentTN.Text; }
+            set { tb_developmentTN.Text = value; }
+        }
+        public string DevelopTP
+        {
+            get { return tb_developmentExtraTP.Text; }
+            set { tb_developmentExtraTP.Text = value; }
+        }
+        public string QuirkTN
+        {
+            get { return tb_quirkTN.Text; }
+            set { tb_quirkTN.Text = value; }
+        }
+        public string PrototypeCost
+        {
+            get { return tb_prototypeCost.Text; }
+            set { tb_prototypeCost.Text = value; }
+        }
+
+        public bool PrototypeBay
+        {
+            get { return cb_noPrototypeBay.Checked; }
+            set { cb_noPrototypeBay.Checked = value; }
+        }
+        public bool RunSims
+        {
+            get { return cb_runSims.Checked; }
+            set { cb_runSims.Checked = value; }
+        }
+        public bool CompileDesigns
+        {
+            get { return cb_compileDesign.Checked; }
+            set { cb_compileDesign.Checked = value; }
+        }
+        public bool ResearchTakeSeven
+        {
+            get { return cb_researchTakeSeven.Checked; }
+            set { cb_researchTakeSeven.Checked = value; }
+        }
+        public bool ResearchCriticalFail
+        {
+            get { return cb_researchCriticalFail.Checked; }
+            set { cb_researchCriticalFail.Checked = value; }
+        }
+        public bool BuildPrototypes
+        {
+            get { return cb_buildPrototypes.Checked; }
+            set { cb_buildPrototypes.Checked = value; }
+        }
+        public bool FieldTest
+        {
+            get { return cb_fieldTest.Checked; }
+            set { cb_fieldTest.Checked = value; }
+        }
+        public bool WorkOutIssues
+        {
+            get { return cb_workOutIssues.Checked; }
+            set { cb_workOutIssues.Checked = value; }
+        }
+        public bool DevelopTakeSeven
+        {
+            get { return cb_developTakeSeven.Checked; }
+            set { cb_developTakeSeven.Checked = value; }
+        }
+        public bool DevelopCriticalFail
+        {
+            get { return cb_developCriticalFail.Checked; }
+            set { cb_developCriticalFail.Checked = value; }
+        }
+
+        public ListBox.SelectedObjectCollection OriginalPosQuirks
+        {
+            get { return clb_originalPosQuirks.SelectedItems; }
+        }
+        public ListBox.SelectedObjectCollection OriginalNegQuirks
+        {
+            get { return clb_originalNegQuirks.SelectedItems; }
+        }
+        public ListBox.SelectedObjectCollection NewPosQuirks
+        {
+            get { return clb_newPosQuirks.SelectedItems; }
+        }
+        public ListBox.SelectedObjectCollection NewNegQuirks
+        {
+            get { return clb_newNegQuirks.SelectedItems; }
+        }
+
+        public DesignCalcForm(Program myClass)
+        {
+            myProgram = myClass;
+            InitializeComponent();
+        }
+        public void SetFieldValues()
+        {
+            UpdateTechField(myProgram.techLevels);
+            UpdateTypeField(myProgram.unitTypes);
+            UpdateProjectField(myProgram.projectTypes);
+            UpdatePlansField(myProgram.planTypes);
+            UpdateOldPosQuirks(myProgram.positiveQuirks);
+            UpdateOldNegQuirks(myProgram.negativeQuirks);
+            UpdateNewPosQuirks(myProgram.positiveQuirks);
+            UpdateNewNegQuirks(myProgram.negativeQuirks);
+        }
+        public void SetInitialValues()
+        {
+            update = false;
+            combo_unitType.SelectedIndex = 0;
+            combo_techLevel.SelectedIndex = 0;
+            combo_projectType.SelectedIndex = 0;
+            combo_plans.SelectedIndex = 0;
+            update = true;
+        }
+
+        private void designCalc_Load(object sender, EventArgs e)
+        {
+            SetFieldValues();
+            SetInitialValues();
+            myProgram.UpdateValues();
+        }
+
+        // Form Updates
+        private void UpdateTechField(List<string> incomingList)
+        {
+            incomingList.ForEach(delegate (string s) { combo_techLevel.Items.Add(s); });
+        }
+        private void UpdateTypeField(List<string> incomingList)
+        {
+            incomingList.ForEach(delegate (string s) { combo_unitType.Items.Add(s); });
+        }
+        private void UpdateProjectField(List<string> incomingList)
+        {
+            incomingList.ForEach(delegate (string s) { combo_projectType.Items.Add(s); });
+        }
+        private void UpdatePlansField(List<string> incomingList)
+        {
+            incomingList.ForEach(delegate (string s) { combo_plans.Items.Add(s); });
+        }
+
+        private void UpdateOldPosQuirks(List<string> incomingList)
+        {
+            incomingList.ForEach(delegate (string s) { clb_originalPosQuirks.Items.Add(s); });
+        }
+        private void UpdateOldNegQuirks(List<string> incomingList)
+        {
+            incomingList.ForEach(delegate (string s) { clb_originalNegQuirks.Items.Add(s); });
+        }
+        private void UpdateNewPosQuirks(List<string> incomingList)
+        {
+            incomingList.ForEach(delegate (string s) { clb_newPosQuirks.Items.Add(s); });
+        }
+        private void UpdateNewNegQuirks(List<string> incomingList)
+        {
+            incomingList.ForEach(delegate (string s) { clb_newNegQuirks.Items.Add(s); });
+        }
+
+        // Event Handlers
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            myProgram.SaveDesignFile();
+        }
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            myProgram.LoadDesignFile();
+        }
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            myProgram.AboutDialog();
+        }
+
+        private void comb_unitType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+        private void comb_techLevel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+        private void comb_projectType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+        private void comb_plans_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+
+        private void tb_prototypeCost_TextChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+
+        private void cb_noPrototypeBay_CheckedChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+        private void cb_runSims_CheckedChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+        private void cb_compileDesign_CheckedChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+        private void cb_researchTakeSeven_CheckedChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+        private void cb_researchCriticalFail_CheckedChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+        private void cb_buildPrototypes_CheckedChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+        private void cb_fieldTest_CheckedChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+        private void cb_workOutIssues_CheckedChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+        private void cb_developTakeSeven_CheckedChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+        private void cb_developCriticalFail_CheckedChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+
+        private void num_runSimsCount_ValueChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+        private void num_compileDesignCount_ValueChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+        private void num_researchTakeSevenCount_ValueChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+        private void num_researchCriticalFailCount_ValueChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+        private void num_buildPrototypeCount_ValueChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+        private void num_fieldTestCount_ValueChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+        private void num_workOutIssuesCount_ValueChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+        private void num_developTakeSevenCount_ValueChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+        private void num_developCriticalFailCount_ValueChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+
+        private void clb_originalPosQuirks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+        private void clb_originalNegQuirks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+        private void clb_newPosQuirks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+        private void clb_newNegQuirks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (update) { myProgram.UpdateValues(); };
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,17 +1,219 @@
 namespace DesignCalculator
 {
-    internal static class Program
+    public class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
+        // Definitions
+        public List<string> techLevels = new();
+        public List<string> unitTypes = new();
+        public List<string> projectTypes = new();
+        public List<string> planTypes = new();
+        public List<string> positiveQuirks = new();
+        public List<string> negativeQuirks = new();
+        public List<int> positiveQuirksValues = new();
+        public List<int> negativeQuirksValues = new();
+
+        public int months = 0;
+        public int spCost = 0;
+        public int sorties = 0;
+        public int totalTPCost = 0;
+        public int baseTN = 0;
+        public int baseTP = 0;
+        public int researchExtraTP = 0;
+        public int researchTN = 0;
+        public int researchSimulationsRun = 0;
+        public int researchDesignsCompiled = 0;
+        public int researchSevensTaken = 0;
+        public int researchCriticalFails = 0;
+        public int developExtraTP = 0;
+        public int developTN = 0;
+        public int developPrototypesBuilt = 0;
+        public int developPrototypesCost = 0;
+        public int developRunFieldTests = 0;
+        public int developIssuesWorkedOut = 0;
+        public int developSevensTaken = 0;
+        public int developCriticalFails = 0;
+
+        public int prototypeBayTP = 0;
+        public int prototypeBayTN = 0;
+        public int existingPlansTP = 0;
+        public int existingPlansTN = 0;
+        public int projectTypeTP = 0;
+        public int projectTypeTN = 0;
+
+        public int runSimulationsTP = 0;
+        public int runSimulationsTN = 0;
+        public int compileDesignsTP = 0;
+        public int compileDesignsTN = 0;
+
+        public int buildPrototypesTP = 0;
+        public int buildPrototypesTN = 0;
+        public int runFieldTestsTP = 0;
+        public int runFieldTestsTN = 0;
+        public int issuesWorkedOutTP = 0;
+        public int issuesWorkedOutTN = 0;
+
+        public int criticalFailsTN = 0;
+        public int criticalFailsTP = 0;
+        public int sevensTakenTN = 0;
+        public int sevensTakenTP = 0;
+
+        public static string str_saveFileFolder = "designs";
+
+        public static DesignCalcForm mainForm;
+
+        private const string str_versionNum = "1.0";
+
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+            Program myClass = new();
+            FindCreateFolders(myClass);
             ApplicationConfiguration.Initialize();
-            Application.Run(new DesignCalcForm());
+            Application.Run(mainForm = new DesignCalcForm(myClass));
+        }
+
+        private static void FindCreateFolders(Program myClass)
+        {
+            try
+            {
+                IEnumerable<string> folderList = Directory.GetDirectories(AppDomain.CurrentDomain.BaseDirectory).Where(s => s.Contains(Program.str_saveFileFolder));
+
+                if (!folderList.Any())
+                {
+                    Directory.CreateDirectory(Program.str_saveFileFolder);
+                };
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error checking/creating save folder.  " + ex.ToString(), "Folder Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            try
+            {
+                    SetDefaultValues(myClass);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error setting default values.  " + ex.ToString(), "Config Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        public void AboutDialog()
+        {
+            try
+            {
+                MessageBox.Show("This tool was coded by Morkath.  It was designed by Sprero to calcuate the design costs for producing new units in a Battletech Campaign game.  Version: " + str_versionNum, "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Showing About.  " + ex.ToString(), "Form Update Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        public void SaveDesignFile()
+        {
+            try
+            {
+                string fileName = "tempName"; // TRAP TODO Fix this to get input from user.
+                string fullFileName = str_saveFileFolder + "\\" + fileName + ".txt";
+                DialogResult saveFile = DialogResult.Yes;
+
+                if (File.Exists(fullFileName))
+                {
+                    saveFile = MessageBox.Show("File exists, Overwrite?", "File Exists", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                }
+
+                if (saveFile == DialogResult.Yes)
+                {
+                    using (StreamWriter wt = new(fullFileName))
+                    {
+                        wt.WriteLine(mainForm.UnitType);
+                        wt.WriteLine(mainForm.Techlevel);
+                        wt.WriteLine(mainForm.Plans);
+                        wt.WriteLine(mainForm.ProjectType);
+
+                        // TRAP TODO add all the other fields that need to get saved.
+
+                        wt.Close();
+                    }
+                }
+                else
+                {
+                    saveFile = DialogResult.No;
+                    MessageBox.Show("File not saved.", "File Not Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Saving Design.  " + ex.ToString(), "Form Update Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        public void LoadDesignFile()
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new();
+                openFileDialog.InitialDirectory = str_saveFileFolder;
+                openFileDialog.Title = "Browse Saved Designs";
+                openFileDialog.DefaultExt = "txt";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt";
+                openFileDialog.CheckFileExists = true;
+                openFileDialog.CheckPathExists = true;
+                openFileDialog.RestoreDirectory = true;
+                openFileDialog.ShowReadOnly = true;
+
+                mainForm.update = false;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamReader rt = new StreamReader(openFileDialog.FileName))
+                    {
+                        mainForm.UnitType = int.Parse(rt.ReadLine());
+                        mainForm.Techlevel = int.Parse(rt.ReadLine());
+                        mainForm.Plans = int.Parse(rt.ReadLine());
+                        mainForm.ProjectType = int.Parse(rt.ReadLine());
+
+                        // TRAP TODO add all the other fields that need to get loaded.
+
+
+                        rt.Close();
+                    }
+                }
+
+                mainForm.update = true;
+                UpdateValues();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Loading Design.  " + ex.ToString(), "Form Update Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void UpdateValues()
+        {
+            CalculateValues();
+        }
+        private static void SetDefaultValues(Program myClass)
+        {
+            // TRAP TODO Set all the initial values for everything.
+
+            try
+            {
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error Message.  " + ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void CalculateValues()
+        {
+            // Update the forms fields based on current data.
+            UpdateUnitType();
+        }
+
+        private void UpdateUnitType()
+        {
+
         }
     }
 }
